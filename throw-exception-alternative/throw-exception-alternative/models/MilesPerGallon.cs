0103c9b45bd58c5912648855bdaa5bf98de376f0
Damon German <damon.german@hyland.com>
2022-08-10 13:41:15 -0500

    made some changes

---
+++ throw-exception-alternative/throw-exception-alternative/models/MilesPerGallon.cs
@@ -0,0 +1,8 @@
+﻿namespace throw_exception_alternative.models;
+
+public record MilesPerGallon
+{
+    public double Miles { get; init; } = 0;
+    public double Gallons { get; init; } = 1;
+    public double? Mpg { get; init; } = null;
+}
